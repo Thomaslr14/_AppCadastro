50484e15831e970e775c3f62472005e64f8b578e
Thomaslr14 <thomaslrodrigues@outlook.com>
2021-03-10 22:26:40 -0300

    Menu-options-made

---
+++ _AppCadastroSeries/Classes/SerieObject.cs
@@ -0,0 +1,10 @@
+using _AppCadastroSeries.Enums;
+
+namespace _AppCadastroSeries.Classes
+{
+    public class SerieObject
+    {
+        private TypeSeries Genero {get;set;}
+
+    }
+}
---
+++ _AppCadastroSeries/Enums/TypeSeries.cs
@@ -0,0 +1,20 @@
+namespace _AppCadastroSeries.Enums
+{
+    public enum TypeSeries
+    {
+        Anime = 0,
+        Ação = 1,
+        Brasileiros = 2,
+        Aventura = 3,
+        Comédia = 4,
+        Drama = 5,
+        Documentários = 6,
+        Familia = 7,
+        Fantasia = 8,
+        Ficção = 9,
+        Infantis = 10,
+        Romanticos = 11,
+        Suspense = 12,
+        Terror = 13,
+    }
+}
