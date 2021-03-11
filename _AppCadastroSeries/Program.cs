e6c85f4881998d2ef78f972d2a6f68e07fe008b9
Thomaslr14 <thomaslrodrigues@outlook.com>
2021-03-10 22:26:25 -0300

    Menu-options-made

--- _AppCadastroSeries/Program.cs
+++ _AppCadastroSeries/Program.cs
@@ -6,7 +6,51 @@ namespace _AppCadastroSeries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var option = ChooseOption();
+            
+            while (option.ToUpper() != "X")
+            {
+                try 
+                {
+                   switch(option)
+                    {
+                        case "1":
+                        break;
+                        
+                        case "2":
+                        break;
+                        
+                        case "3":
+                        break;
+                        
+                        case "4":
+                        break;
+                        
+                        default:
+                            throw new ArgumentNullException();
+                    }
+                
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Informe uma opção valida!");
+                    option = ChooseOption();
+                }
+            }
+            Console.WriteLine("Obrigado por utilizar o Dio Stream!");
+        }
+
+        static string ChooseOption()
+        {
+            Console.WriteLine("Bem vindo ao DIO Stream!");
+            Console.WriteLine("1 - Listar séries");
+            Console.WriteLine("2 - Inserir série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("X - SAIR");
+            Console.WriteLine("Escolha a opção desejada");
+            string opt = Console.ReadLine();
+            return opt;
         }
     }
 }
